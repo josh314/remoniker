fe36ff6d61e37d9d29bd3fb335a4e456ef929ea3
Joshua Davis <josh314@gmail.com>
2015-11-17 17:00:29 -0500

    Input fields added for search and replace strings.

--- src/main/java/ninja/joshdavis/AppFrame.java
+++ src/main/java/ninja/joshdavis/AppFrame.java
@@ -13,30 +13,44 @@ import java.io.IOException;
 public class AppFrame extends JFrame {
     private FileListPane srcFileListPane;
     private FileListPane destFileListPane;
-    private SearchInput searchInput;
     private Editor editor;
-        
-    public void setDirectory(String dirName) {
-        File dir = new File(dirName);
-        /*
-        if(!dir.isDirectory()) {
-            throw new IOException("File is not a directory.");
-        }
-        */
-        String[] contents = dir.list();
-        String srcText = "";
-        for(String file: contents) {
-            srcText = srcText + file + "\n";//Portable line ending?
-        }
-        srcFileListPane.setText(srcText);
+    private TextField searchInput;
+    private TextField replaceInput;
 
+    private File currentDir;
+
+    private class InputListener implements ActionListener {
+        public void actionPerformed(ActionEvent ev) {
+            //TODO: sanitize inputs
+            
+            String searchInputString = searchInput.getText();
+            String replaceInputString = replaceInput.getText();
+            editor.setSearchString(searchInputString);
+            editor.setReplaceString(replaceInputString);
+            updateFilePanes();
+        }
+    }
+    
+    private void setCurrentDir(String filename) {
+        File file = new File(filename);
+        if(file.isDirectory()) {
+            currentDir = file;
+        }
+        updateFilePanes();
+    }
+    
+    private void updateFilePanes() {
+        String[] contents = currentDir.list();
+        String srcText = "";
         String destText = "";
         for(String file: contents) {
             String newName = editor.edit(file);
             if(newName != null && !newName.isEmpty()) {
+                srcText = srcText + file + "\n";
                 destText = destText + newName + "\n";
             }
         }
+        srcFileListPane.setText(srcText);
         destFileListPane.setText(destText);
     }
     
@@ -51,13 +65,17 @@ public class AppFrame extends JFrame {
         destFileListPane = new FileListPane(); 
         add(destFileListPane);        
         //TODO: set handlers - update from directory contents   
-
-        searchInput = new SearchInput("");
-        
-        //
         editor = new Editor();
 
-        setDirectory("/");
+        setCurrentDir("/");
+        
+        searchInput = new TextField(20);
+        replaceInput = new TextField(20);
+        ActionListener inputListener = new InputListener();
+        searchInput.addActionListener(inputListener);
+        replaceInput.addActionListener(inputListener);
+        add(searchInput);
+        add(replaceInput);
     }
 
 }
--- src/main/java/ninja/joshdavis/SearchInput.java
+++
@@ -1,8 +0,0 @@
-package ninja.joshdavis;
-
-public class SearchInput {
-
-    public SearchInput(String s) {
-        //Sanitize string
-    }
-}
