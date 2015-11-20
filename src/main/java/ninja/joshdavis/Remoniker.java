1cddd1a6aabab42c961be317c3cf7a9e11e923c6
Joshua Davis <josh314@gmail.com>
2015-11-20 14:12:10 -0500

    Updating the Swing GUI from the Event Dispatching Thread.

--- src/main/java/ninja/joshdavis/Remoniker.java
+++ src/main/java/ninja/joshdavis/Remoniker.java
@@ -1,16 +1,25 @@
 package ninja.joshdavis;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 /**
  * Launcher for the graphical interface
  *
  */
 public class Remoniker {
-    public static void main(String[] args) {
+    private static void createAndShowGUI() {
         AppFrame frame = new AppFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(640,480);
         frame.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    createAndShowGUI();
+                }
+            });
     }
 }
